b678b4c126335eed4b249fcc873ce3026b54f07e
r0ggle <ruairigann@hotmail.com>
2018-06-24 16:03:59 +0100

    fixed things

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.onload = function()
 {
 	"use strict";
-	var title = (t.$('head-title').textContent) ?
-	t.$('head-title').textContent :
-	t.$('head-title').innerText;
+	var title = (T.$('head-title').textContent) ?
+	T.$('head-title').textContent :
+	T.$('head-title').innerText;
 	console.log('window loaded: ' + title);
 };
