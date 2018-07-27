85498283fa6b000465376975b5a260aac6443b7b
r0ggle <ruairigann@hotmail.com>
2018-07-27 14:32:32 +0100

    what a mess, but it does kinda work

--- index/timefiles.php
+++ index/timefiles.php
@@ -1,30 +1,43 @@
 <?php
 	require("../../inc/config.php");
 	include(INC."/header.html");
-
-	$mysqli = new mysqli("localhost", "root", "1620", "timefiles");
-	if ($mysqli->connect_errno) {
-	    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	    exit();
-	}
+	require(INC."/mysqli.php");
 
 	$user_id = "1";
+	$mysql_time = date("Y-m-d H:i:s");
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		if ($_POST["time-start"] != ""
-		&& $_POST["time-end"] != ""
-		&& $_POST["time-date"]!= ""
-		&& $_POST["time-activity"] != "") {
-			$time_start = $_POST["time-date"].' '.$_POST["time-start"].':00';
-			$time_end = $_POST["time-date"].' '.$_POST["time-end"].':00';
-			$task_id = $_POST["time-activity"];
-			$q = "INSERT INTO times (task_id, time_start, time_end) VALUES ('$task_id', '$time_start', '$time_end')";
+
+		if ($_POST['la-submit']) { // log new task
+
+			if ($_POST['la-time-date'] == '') {
+				$la_date = date("Y-m-d");
+			} else {
+				$la_date = date("Y-m-d", $_POST['la-time-date']);
+			}
+
+			$time_start = ($_POST["la-time-start"] == "" ?
+			$mysql_time : ($la_date.' '.$_POST["la-time-start"].':00'));
+
+			$time_end = ($_POST["la-time-end"] == "" ?
+			"0000-00-00 00:00:00" : ($la_date.' '.$_POST["la-time-end"].':00'));
+		
+			if ($_POST['la-time-task'] == '') exit();
+			$task_id = $_POST["la-time-task"];
+
+			/*$q = "INSERT INTO times (task_id, time_start, time_end) VALUES ('$task_id', '$time_start', '$time_end')";
 			if (!$mysqli->query($q)) {
 				echo "Faild to insert row: " . $mysqli->errno;
 				echo $q;
-			}
+			}*/
+		} else if ($_POST['ct-submit']) { // complete task
+
+			if (!$_POST['ct-id']) exit();
+			$ct_id = $_POST['ct-id'];
+
+			// complete the incomplete task:
+			$r = $mysqli->query("UPDATE times SET time_end=CURRENT_TIMESTAMP() WHERE id=$ct_id");
 		}
-	} else {
 		
 	}
 ?>
@@ -36,27 +49,52 @@
 			<div>
 				<p>Because time flies...</p>
 				<!-- start of time files area -->
-				<form name="time-form" id="tf-form" action="#" method="post">
-					<fieldset>
-						<legend>Log Activity</legend>
-						<label for="time-start">Start Time</label>
-						<input type="time" name="time-start">
-						<label for="time-end">End Time</label>
-						<input type="time" name="time-end">
-						<input type="date" name="time-date">
-						<select name="time-activity">
 <?php
-	$r = $mysqli->query("SELECT id, name FROM tasks WHERE user_id=$user_id");
-	while ($row = $r->fetch_assoc()) {
-		echo '<option value="'
-		. $row["id"] . '">' . $row["name"] .
-		'</option>';
-	}
+	$r = $mysqli->query("SELECT ti.id, ti.time_start, ta.name FROM times as ti LEFT JOIN tasks as ta ON ti.task_id = ta.id WHERE ta.user_id='$user_id' AND ti.time_end = '0000-00-00 00:00:00';");
+
+	if (mysqli_num_rows($r) != 0) {
 ?>
-						</select>
-						<input type="submit" name="Submit" value="Submit">
+				<form name="complete-task" action="#" method="post">
+					<fieldset>
+						<legend>Complete Task</legend>
+						<p>You have an ongoing task:
+<?php
+	$row = $r->fetch_assoc();
+	//$time_id = $row['ti.id'];
+	echo $row['name'].', started:<br>'.$row['time_start'];
+	echo '<input type="hidden" name="ct-id" value="'.$row['id'].'">';
+?>
+						</p>
+						<input type="submit" name="ct-submit" value="Complete">
 					</fieldset>
 				</form>
+<?php
+} else {
+?>
+				<form name="log-task" action="#" method="post">
+					<fieldset>
+						<legend>Log task</legend>
+						<label for="la-time-start">Start Time</label>
+						<input type="time" name="la-time-start">
+						<label for="la-time-end">End Time</label>
+						<input type="time" name="la-time-end">
+						<input type="date" name="la-time-date">
+						<select name="la-time-task">
+	<?php
+		$r = $mysqli->query("SELECT id, name FROM tasks WHERE user_id=$user_id");
+		while ($row = $r->fetch_assoc()) {
+			echo '<option value="'
+			. $row["id"] . '">' . $row["name"] .
+			'</option>';
+		}
+	?>
+						</select>
+						<input type="submit" name="la-submit" value="Submit">
+					</fieldset>
+				</form> <!-- end of log-task form -->
+<?php
+	}
+?>
 				<div id="tf-wrapper">
 					<h3>August</h3>
 					<div id="tf-month">
