3256c39b5ccb5eed9af588ca34ce0313548c6d36
r0ggle <ruairigann@hotmail.com>
2018-07-27 17:08:11 +0100

    made site default UTC, made month layout print the actual number of days in the current month, even though it says it's always august..

--- index/timefiles.php
+++ index/timefiles.php
@@ -8,7 +8,7 @@
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-		if ($_POST['la-submit']) { // log new task
+		if (isset($_POST['la-submit'])) { // log new task
 
 			if ($_POST['la-time-date'] == '') {
 				$la_date = date("Y-m-d");
@@ -25,12 +25,12 @@
 			if ($_POST['la-time-task'] == '') exit();
 			$task_id = $_POST["la-time-task"];
 
-			/*$q = "INSERT INTO times (task_id, time_start, time_end) VALUES ('$task_id', '$time_start', '$time_end')";
+			$q = "INSERT INTO times (task_id, time_start, time_end) VALUES ('$task_id', '$time_start', '$time_end')";
 			if (!$mysqli->query($q)) {
 				echo "Faild to insert row: " . $mysqli->errno;
 				echo $q;
-			}*/
-		} else if ($_POST['ct-submit']) { // complete task
+			}
+		} else if (isset($_POST['ct-submit'])) { // complete task
 
 			if (!$_POST['ct-id']) exit();
 			$ct_id = $_POST['ct-id'];
@@ -61,7 +61,7 @@
 <?php
 	$row = $r->fetch_assoc();
 	//$time_id = $row['ti.id'];
-	echo $row['name'].', started:<br>'.$row['time_start'];
+	echo $row['name'].', started:<br>'.$row['time_start'].' UTC';
 	echo '<input type="hidden" name="ct-id" value="'.$row['id'].'">';
 ?>
 						</p>
@@ -98,38 +98,14 @@
 				<div id="tf-wrapper">
 					<h3>August</h3>
 					<div id="tf-month">
-						<div class="tf-month-day">1</div>
-						<div class="tf-month-day">2</div>
-						<div class="tf-month-day">3</div>
-						<div class="tf-month-day">4</div>
-						<div class="tf-month-day">5</div>
-						<div class="tf-month-day">6</div>
-						<div class="tf-month-day">7</div>
-						<div class="tf-month-day">8</div>
-						<div class="tf-month-day">9</div>
-						<div class="tf-month-day">10</div>
-						<div class="tf-month-day">11</div>
-						<div class="tf-month-day">12</div>
-						<div class="tf-month-day">13</div>
-						<div class="tf-month-day">14</div>
-						<div class="tf-month-day">15</div>
-						<div class="tf-month-day">16</div>
-						<div class="tf-month-day">17</div>
-						<div class="tf-month-day">18</div>
-						<div class="tf-month-day">19</div>
-						<div class="tf-month-day">20</div>
-						<div class="tf-month-day">21</div>
-						<div class="tf-month-day">22</div>
-						<div class="tf-month-day">23</div>
-						<div class="tf-month-day">24</div>
-						<div class="tf-month-day">25</div>
-						<div class="tf-month-day">26</div>
-						<div class="tf-month-day">27</div>
-						<div class="tf-month-day">28</div>
-						<div class="tf-month-day">29</div>
-						<div class="tf-month-day">30</div>
-						<div class="tf-month-day">31</div>
-						<div class="tf-month-day">32</div>
+<?php
+	$r = $mysqli->query("SELECT DAY(LAST_DAY(CURRENT_TIMESTAMP()))");
+		$row = $r->fetch_array(MYSQLI_NUM);
+		$days_in_month = $row[0];
+		for ($i=1; $i <= $days_in_month; $i++) {
+			echo '<div class="tf-month-day">'.$i.'</div>';
+		}
+?>
 					</div> <!-- end of tf-grid -->
 					<div id="tf-month-nav"> &lt; July September &gt; </div>
 					<div id="tf-day">
